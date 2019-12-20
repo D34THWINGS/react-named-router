40305fcb479c8ec60fc571c9592a991393a964f5
d34thwings <d34thwings@gmail.com>
2019-12-20 14:53:33 +0000

    🐛 Fix not working withNamedRouting HOC

--- src/withNamedRouting.tsx
+++ src/withNamedRouting.tsx
@@ -12,10 +12,12 @@ export interface WithNamedRoutingProps {
 export function withNamedRouting<TProps extends WithNamedRoutingProps>(
   Component: React.ComponentType<WithNamedRoutingProps>,
 ) {
-  const namedRouting = useNamedRouting();
-  const WrappedComponent = (props: Omit<TProps, keyof WithNamedRoutingProps>) => (
-    <Component {...props} namedRouting={namedRouting} />
-  );
+  const WrappedComponent = (props: Omit<TProps, keyof WithNamedRoutingProps>) => {
+    const namedRouting = useNamedRouting();
+    return (
+      <Component {...props} namedRouting={namedRouting} />
+    );
+  };
   WrappedComponent.displayName = `WithRouting(${Component.displayName || Component.name})`;
   return WrappedComponent;
 }
